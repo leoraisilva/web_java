32e3424dd82a6dc5f95d5f1f28587ab21c75f4a2
leoraisilva <lraimundo409@gmail.com>
2024-07-02 14:47:21 -0300

    alterando os arquivos

---
+++ src/main/java/com/web/web_vcard/WebVcardApplication.java
@@ -0,0 +1,13 @@
+package com.web.web_vcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/web_vcard/WebVcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.web_vcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebVcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
